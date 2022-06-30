55153f7d4d772a8e216ae4853bffc23a17d87fb2
omoikanelulu <r.omoikane@gmail.com>
2022-06-30 11:50:34 +0900

    jsも書くよ

--- shared_todo_lists/login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('../class/util/Security.php');
-Security::session();
-
-unset($_SESSION['user'], $_SESSION['err'], $_SESSION['success'], $_SESSION['post'], $_SESSION['data']);
-header('Location:./index.php');
-exit();
